cf6a5b115d57adcc468659a6638456875246a010
zsp19931222 <872126510@qq.com>
2017-07-13 17:21:03 +0800

    工具类

---
+++ app/src/main/java/zsp/mytool/ToastUtils.java
@@ -0,0 +1,15 @@
+package zsp.mytool;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/3/16 0016.
+ */
+
+public class ToastUtils {
+    public static void Toast(Context activity, String message){
+        Toast.makeText(activity,message,Toast.LENGTH_SHORT).show();
+    }
+}
